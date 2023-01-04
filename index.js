4323bc88c6407efcc82e9197444853ed1a419d7c
Reino Pesonen <reino.pesonen@gmail.com>
2023-01-05 01:22:50 +0200

    Add intents for client

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: ["GUILDS", "USER"] });
 var CronJob = require('cron').CronJob;
 const fs = require('fs')
 
