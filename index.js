3d9ec8223df6efb56f2d502f3391a327cf845fbf
Reino Pesonen <reino.pesonen@gmail.com>
2024-09-05 13:08:05 +0300

    Add error handling to check function

--- index.js
+++ index.js
@@ -80,72 +80,76 @@ var Check = new CronJob(config.cron, async function () {
     tempData.channels.map(async function (chan, i) {
         if (!chan.ChannelName) return;
 
-        let StreamData = await Stream.getData(chan.ChannelName, tempData.twitch_clientID, tempData.authToken);
-        if (StreamData.data.length == 0) return
+        try {
+            let StreamData = await Stream.getData(chan.ChannelName, tempData.twitch_clientID, tempData.authToken);
+            if (StreamData.data.length == 0) return;
 
-        StreamData = StreamData.data[0];
-        const ChannelData = await Channel.getData(chan.ChannelName, tempData.twitch_clientID, tempData.authToken);
-        if (!ChannelData) return;
+            StreamData = StreamData.data[0];
+            const ChannelData = await Channel.getData(chan.ChannelName, tempData.twitch_clientID, tempData.authToken);
+            if (!ChannelData) return;
 
-        var message = `Hey @everyone, ${StreamData.user_name} is now live on https://www.twitch.tv/${StreamData.user_login} Go check it out!`;
-        var owner = false;
-        if (StreamData.user_login === 'rebepoi') {
-            owner = true;
-        }
-
-        var SendEmbed = {
-            "title": StreamData.title,
-            "url": `https://www.twitch.tv/${StreamData.user_login}`,
-            "color": 6570404,
-            "author": {
-                "name": `${StreamData.user_name}`,
-                "icon_url": `${ChannelData.thumbnail_url}`,
-                "url": `https://www.twitch.tv/${StreamData.user_login}`,
-            },
-            "fields": [
-                {
-                    "name": "Playing:",
-                    "value": StreamData.game_name,
-                    "inline": true
-                },
-                {
-                    "name": "Viewers:",
-                    "value": StreamData.viewer_count,
-                    "inline": true
-                },
-                (chan.DiscordServer ? {
-                    "name": "Discord Server:",
-                    "value": `[Join here](${chan.DiscordServer})`
-                } : {
-                    "name": "** **",
-                    "value": "** **"
-                })
-            ],
-            "footer": {
-                "text": owner ? "Let us fail again!" : ""
-            },
-            "image": {
-                "url": `https://static-cdn.jtvnw.net/previews-ttv/live_user_${StreamData.user_login}-640x360.jpg?cacheBypass=${(Math.random()).toString()}`
-            },
-            "thumbnail": {
-                "url": `${ChannelData.thumbnail_url}`
+            var message = `Hey @everyone, ${StreamData.user_name} is now live on https://www.twitch.tv/${StreamData.user_login} Go check it out!`;
+            var owner = false;
+            if (StreamData.user_login === 'rebepoi') {
+                owner = true;
             }
-        };
 
-        const sendChannel = client.guilds.cache.get(config.DiscordServerId).channels.cache.get(config.channelID);
+            var SendEmbed = {
+                "title": StreamData.title,
+                "url": `https://www.twitch.tv/${StreamData.user_login}`,
+                "color": 6570404,
+                "author": {
+                    "name": `${StreamData.user_name}`,
+                    "icon_url": `${ChannelData.thumbnail_url}`,
+                    "url": `https://www.twitch.tv/${StreamData.user_login}`,
+                },
+                "fields": [
+                    {
+                        "name": "Playing:",
+                        "value": StreamData.game_name,
+                        "inline": true
+                    },
+                    {
+                        "name": "Viewers:",
+                        "value": StreamData.viewer_count,
+                        "inline": true
+                    },
+                    (chan.DiscordServer ? {
+                        "name": "Discord Server:",
+                        "value": `Join here`
+                    } : {
+                        "name": "** **",
+                        "value": "** **"
+                    })
+                ],
+                "footer": {
+                    "text": owner ? "Let us fail again!" : ""
+                },
+                "image": {
+                    "url": `https://static-cdn.jtvnw.net/previews-ttv/live_user_${StreamData.user_login}-640x360.jpg?cacheBypass=${(Math.random()).toString()}`
+                },
+                "thumbnail": {
+                    "url": `${ChannelData.thumbnail_url}`
+                }
+            };
 
-        if (chan.twitch_stream_id == StreamData.id) {
-            sendChannel.messages.fetch(chan.discord_message_id).then(msg => {
-                msg.edit({ embeds: [SendEmbed] });
-            });
-        } else {
-            await sendChannel.send({ content: message, embeds: [SendEmbed] }).then(msg => {
-                const channelObj = tempData.channels[i];
-                channelObj.discord_message_id = msg.id;
-                channelObj.twitch_stream_id = StreamData.id;
-            });
+            const sendChannel = client.guilds.cache.get(config.DiscordServerId).channels.cache.get(config.channelID);
+
+            if (chan.twitch_stream_id == StreamData.id) {
+                sendChannel.messages.fetch(chan.discord_message_id).then(msg => {
+                    msg.edit({ embeds: [SendEmbed] });
+                });
+            } else {
+                await sendChannel.send({ content: message, embeds: [SendEmbed] }).then(msg => {
+                    const channelObj = tempData.channels[i];
+                    channelObj.discord_message_id = msg.id;
+                    channelObj.twitch_stream_id = StreamData.id;
+                });
+            }
+            fs.writeFileSync('./config.json', JSON.stringify(tempData));
+        } catch (error) {
+            console.error('Error fetching stream data:', error);
         }
-        fs.writeFileSync('./config.json', JSON.stringify(tempData));
     });
 });
 
